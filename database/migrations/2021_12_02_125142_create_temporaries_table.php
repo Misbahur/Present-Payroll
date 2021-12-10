ef026de142cee330eea115829275c47a78d3313a
hadziqtamami12 <hadziqtamami@gmail.com>
2021-12-10 14:33:25 +0700

    ubah status migration temporary

--- database/migrations/2021_12_02_125142_create_temporaries_table.php
+++ database/migrations/2021_12_02_125142_create_temporaries_table.php
@@ -16,8 +16,8 @@ class CreateTemporariesTable extends Migration
         Schema::create('temporaries', function (Blueprint $table) {
             $table->id();
             $table->date('tanggal');
-            // $table->enum('status', ['in', 'out']);
-            $table->string('status');
+            $table->enum('status', ['in-lembur-harian', 'out-telat-harian', 'out-absen-harian', 'in-bonus-mingguan', 'in-bonus-bulanan', 'in-bonus-libur-masuk' ]);
+            // $table->string('status');
             $table->integer('nominal');
             // $table->string('keterangan');
             // $table->unsignedBigInteger('lembur_id');
