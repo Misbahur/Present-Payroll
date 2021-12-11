2705bab5e9e9f52569031e12b392ef003ac401ca
hadziqtamami12 <hadziqtamami@gmail.com>
2021-12-12 02:14:27 +0700

    Seeder Pegawai Gocay

--- database/seeders/PegawaiSeeder.php
+++ database/seeders/PegawaiSeeder.php
@@ -18,21 +18,666 @@ class PegawaiSeeder extends Seeder
         // Pegawai::factory()->times(10)->create();
         \App\Models\Pegawai::insert([
             [ 
-                'nama' => 'Rina',
+                'nama' => 'kharisma',
                 'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
             ],
             [ 
-                'nama' => 'Aisyah',
+                'nama' => 'Sofi',
                 'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
             ],
             [ 
-                'nama' => 'Deni',
-                'jabatan_id' => '2',
+                'nama' => 'iis',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
             ],
             [ 
-                'nama' => 'Ghofur',
+                'nama' => 'elim',
                 'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
             ],
+            [ 
+                'nama' => 'icha',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'nadin',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'yayak',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'aisyah',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'diah',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'mujib',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'husen',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'windy',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'nabila',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'ida',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'lia',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'nuri',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'enggar',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'reta',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'via',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'difah',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'yanto',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'wafi',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'dar',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'bahri',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'halim',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'anggi',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'doni',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'prayudi',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'zen',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'ely',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'bas',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'aldi',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'mansur',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'hasyim',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'dony',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'alfis',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'sipol',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'zaky',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'umam',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'aisyah',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'dini',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'ila',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'fina',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'regita',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'ina',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'ike',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+
+            [ 
+                'nama' => 'nia',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'sony',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'khoirul',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'uus',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'fajar',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'rosy',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'sitqi',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'yoga',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'arif',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'miftah',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+
+            [ 
+                'nama' => 'yuli',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'nur',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'wildan',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'sipol',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'wafa',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'rizal',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'solihin',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'fiky',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+
+            [ 
+                'nama' => 'ifnay',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'ikhlas',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'fian',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'popo',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'risky',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'subaida',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'putri',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'cindy',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            [ 
+                'nama' => 'dhea',
+                'jabatan_id' => '1',
+                'nik' => '',
+                'alamat' => '',
+                'tanggal_masuk' =>'',
+                'tanggal_lahir' =>'',
+
+            ],
+            
+
         ]);
     }
 }
