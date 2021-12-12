aec6c60812efcc6bb756b27a30365de30817ef7e
hadziqtamami12 <hadziqtamami@gmail.com>
2021-12-12 11:17:07 +0700

    tes kernel cronjob

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -34,7 +34,8 @@ class Kernel extends ConsoleKernel
         $schedule->command('bonus:week')->weekly();
         $schedule->command('bonus:liburmasuk')->weekly();
         $schedule->command('bonus:month')->monthly();
-        $schedule->command('kehadiran:log')->everyTenMinutes();
+        $schedule->command('kehadiran:log')->everyMinute();
+        // $schedule->command('kehadiran:log')->everyTenMinutes();
         $schedule->command('kehadiran:dummy')->monthly();
         $schedule->command('kehadiran:absen')->daily();
     }
