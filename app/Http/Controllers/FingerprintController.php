4271f81043fc16e80b2b51106ff40ad614488890
hadziqtamami12 <hadziqtamami@gmail.com>
2021-12-20 19:02:35 +0700

    tes finger gocay 2

--- app/Http/Controllers/FingerprintController.php
+++ app/Http/Controllers/FingerprintController.php
@@ -339,7 +339,7 @@ class FingerprintController extends Controller
 
         $pegawais = Pegawai::all();
         foreach ($pegawais as $item):
-            $zk2->setUser($item->id, $item->id, $item->nama, strtolower($item->nama));
+            $zk2->setUser($item->id, $item->id, $item->nama, $item->nama);
         endforeach;
 
         return redirect()->back();
