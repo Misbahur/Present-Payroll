7039331e4c44aa2c98a5b4eff00f7b4068d0bcfe
hadziqtamami12 <hadziqtamami@gmail.com>
2022-02-08 12:35:18 +0700

    penyesuaian kehadiran bulanan terakhir

--- config/snappy.php
+++ config/snappy.php
@@ -35,8 +35,8 @@ return [
 
     'pdf' => array(
         'enabled' => true,
-        'binary' => base_path('vendor\wemersonjanuario\wkhtmltopdf-windows\bin\64bit\wkhtmltopdf'),
-        // 'binary' => '"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf"',
+        // 'binary' => base_path('vendor\wemersonjanuario\wkhtmltopdf-windows\bin\64bit\wkhtmltopdf'),
+        'binary' => '"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf"',
         'timeout' => false,
         'options' => array(),
         'env' => array(),
